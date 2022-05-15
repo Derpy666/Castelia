e8265f113cbbfed2d2e09536d7fe4e19194665b9
Derpy <37512753+Derpy666@users.noreply.github.com>
2022-05-15 10:03:49 +0300

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -2,10 +2,10 @@ require("dotenv").config()
 
 // MySQL database
 module.exports.mySQL = {
-  host: process env.MYSQL_HOST,
-  user: process env.MYSQL_USER,
-  password: process env.MYSQL_PASS,
-  database: process env.MYSQL_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_NAME,
 };
 
 // Discord bot config
