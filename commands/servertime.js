63d972779b15c92ba876b4fa6cae66a20f857d1e
Derpy <37512753+Derpy666@users.noreply.github.com>
2022-05-15 09:26:29 +0300

    Update servertime.js

--- commands/servertime.js
+++ commands/servertime.js
@@ -6,11 +6,11 @@ module.exports.run = async (bot, message) => {
     message.delete();
   }
   const serverTime = moment().utc().format('dddd, MMMM Do, YYYY, h:mm:ss A');
-  const servedEmbed = new Discord.RichEmbed()
+  const servedEmbed = new Discord.MessageEmbed()
     .setColor('#00ab66')
     .setDescription(`The current in-game server time is **${serverTime}**.`);
 
-  message.author.send(servedEmbed);
+  message.author.send({ embeds: [servedEmbed] });
 };
 
 module.exports.help = {
